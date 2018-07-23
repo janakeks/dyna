08d6fa49542b7a131087259dd44acb10beb7dc61
Alf Drollinger <alf@drollinger.info>
2018-07-24 03:10:16 +0800

    Created reasonable Copyright footer

--- footer.php
+++ footer.php
@@ -13,15 +13,16 @@
 
 <footer id="colophon" class="site-footer">
 	<div class="site-info">
-		<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_s' ) ); ?>"><?php
-			/* translators: %s: CMS name, i.e. WordPress. */
-			printf( esc_html__( 'Proudly powered by %s', 'gutenbergtheme' ), 'WordPress' );
-		?></a>
-		<span class="sep"> | </span>
-		<?php
-			/* translators: 1: Theme name, 2: Theme author. */
-			printf( esc_html__( 'Theme: %s', 'gutenbergtheme' ), '<a href="https://github.com/WordPress/gutenberg-starter-theme/">Gutenberg</a>' );
-		?>
+        Copyright <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?> <span class="sep"> | </span>
+
+		<?php _e('Created with', 'dyna'); ?>
+
+        <a href="<?php echo esc_url( __( 'https://wordpress.org/', '_s' ) ); ?>">WordPress</a>
+
+		<?php _e('and', 'dyna'); ?>
+
+        <a href="<?php echo esc_url( __( 'https://dyna.press/', '_s' ) ); ?>">Dyna</a>
+
 	</div><!-- .site-info -->
 </footer><!-- #colophon -->
 </div><!-- #page -->
