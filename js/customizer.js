931b2f6ab711b3b8dbf38911cf97b834825babcd
Alf Drollinger <alf@drollinger.info>
2018-07-25 12:17:47 +0800

    added explanations

--- js/customizer.js
+++ js/customizer.js
@@ -5,6 +5,8 @@
  *
  * Contains handlers to make Theme Customizer preview reload changes asynchronously.
  */
+
+/** Defining global variable for Eslint / Scrutinizer */
 /** global: wp */
 
 ( function( $ ) {
