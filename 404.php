118535bf129ec78befe848bce1cf35e228b0527f
Alf Drollinger <alf@drollinger.info>
2018-08-18 00:25:37 +0700

    Removed monthly archives from 404
    Unneccesary feature with bad code smell

--- 404.php
+++ 404.php
@@ -42,10 +42,6 @@ get_header();
 					</div><!-- .widget -->
 
 					<?php
-					/* translators: %1$s: smiley */
-					$dyna_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', '_s' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$dyna_archive_content" );
-
 					the_widget( 'WP_Widget_Tag_Cloud' );
 					?>
 
